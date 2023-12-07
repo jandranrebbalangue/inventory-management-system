51905b558fdb6433dcc83b9472183291784c168c
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-12-07 16:55:04 +0800

    Add session for requesting api

--- packages/functions/src/lambda.ts
+++ packages/functions/src/lambda.ts
@@ -1,8 +1,11 @@
 import { ApiHandler } from "sst/node/api";
+import { useSession } from "sst/node/auth";
 
 export const handler = ApiHandler(async (_evt) => {
+  const session = useSession();
+  if (session.type !== "user") throw new Error("Not authenticated");
   return {
     statusCode: 200,
-    body: `Hi Mom ${new Date().toISOString()}`,
+    body: session.properties.userId,
   };
 });
