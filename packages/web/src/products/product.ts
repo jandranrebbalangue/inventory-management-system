1732525c4af97c6ee4719632aa856255fc089dd0
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-12-11 15:13:49 +0800

    Add pagination

--- packages/web/src/products/product.ts
+++ packages/web/src/products/product.ts
@@ -15,4 +15,60 @@ export const products: Product[] = [
     quantity: 125,
     productName: "Test item 2",
   },
+
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 3",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 4",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 5",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 6",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 7",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 8",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 9",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 10",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 11",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 12",
+  },
+  {
+    productId: "489e1d42",
+    quantity: 125,
+    productName: "Test item 13",
+  },
 ];
