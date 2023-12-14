027373861745928a836bad9abe424c695d28f653
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-12-14 20:07:46 +0800

    Change fetch to axios

--- packages/web/src/utils/fetcher.ts
+++ packages/web/src/utils/fetcher.ts
@@ -1,10 +1,12 @@
+import axios from "axios";
 import { API_TOKEN } from "@/constants";
 
 const token = localStorage.getItem(API_TOKEN);
 export const fetcher = <T>(endpoint: string): Promise<T> =>
-  fetch(`${import.meta.env.VITE_API_ENDPOINT}${endpoint}`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  }).then((res) => res.json());
+  axios
+    .get(`${import.meta.env.VITE_API_ENDPOINT}${endpoint}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then((res) => res.data);
