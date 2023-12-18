615acd1344af4f51ae63b6a78d151900b8d880ec
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-12-18 14:13:03 +0800

    Make modal

--- packages/web/src/components/Dialog.tsx
+++ packages/web/src/components/Dialog.tsx
@@ -1,18 +1,4 @@
-import * as z from "zod";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { mutate } from "swr";
 import { Button } from "@/components/ui/button";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { Input } from "@/components/ui/input";
 import {
   Dialog,
   DialogContent,
@@ -20,44 +6,17 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
-import post from "@/api/post";
-import { useToast } from "./ui/use-toast";
+import React from "react";
 
-const Modal = ({ label }: { label: string }) => {
-  const { toast } = useToast();
-
-  const formSchema = z.object({
-    productCode: z.string(),
-    productName: z.string(),
-    quantity: z.union([z.string(), z.number()]).pipe(z.coerce.number()),
-  });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: {
-      productCode: "",
-      productName: "",
-      quantity: 0,
-    },
-  });
-
-  const onSubmit = async (data: z.infer<typeof formSchema>) => {
-    const product = (await post({
-      apiName: "/products",
-      data,
-    })) as string | number;
-    if (typeof product === "number") {
-      toast({
-        description: "Add Product successfully",
-      });
-      mutate("/products");
-    } else {
-      toast({
-        description: product,
-      });
-    }
-  };
-
+const Modal = ({
+  label,
+  children,
+  title,
+}: {
+  title: string;
+  label: string;
+  children: string | React.ReactNode;
+}) => {
   return (
     <Dialog>
       <DialogTrigger asChild>
@@ -65,57 +24,9 @@ const Modal = ({ label }: { label: string }) => {
       </DialogTrigger>
       <DialogContent>
         <DialogHeader>
-          <DialogTitle>Add Product</DialogTitle>
+          <DialogTitle>{title}</DialogTitle>
         </DialogHeader>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={form.control}
-              name="productCode"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Product Code</FormLabel>
-                  <FormControl>
-                    <Input placeholder="product code" {...field} />
-                  </FormControl>
-                  <FormDescription>This is your product code</FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="productName"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Product Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="product name" {...field} />
-                  </FormControl>
-                  <FormDescription>This is your product name</FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <FormField
-              control={form.control}
-              name="quantity"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Quantity</FormLabel>
-                  <FormControl>
-                    <Input placeholder="quantity" {...field} type="number" />
-                  </FormControl>
-                  <FormDescription>This is your quantity</FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <Button type="submit">Submit</Button>
-          </form>
-        </Form>
+        {children}
       </DialogContent>
     </Dialog>
   );
