703d953f0800c32f711e7d12b8fa345a6be1f0a2
Jan Dranreb Balangue <jandranrebbalangue@gmail.com>
2023-12-18 13:39:04 +0800

    Add type unknown for token

--- packages/web/src/utils/fetcher.ts
+++ packages/web/src/utils/fetcher.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const fetcher = async <T>(
   endpoint: string,
-  token: string,
+  token: string | unknown,
 ): Promise<T> => {
   const res = await axios.get(
     `${import.meta.env.VITE_API_ENDPOINT}${endpoint}`,
